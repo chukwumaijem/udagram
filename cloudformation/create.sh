c6a32fa6ba0611a58a4135e294649ddfa5742315
Chukwuma Zikora <chukwuma.zikora@gmail.com>
2022-06-17 12:44:53 +0100

    update stacks

--- cloudformation/create.sh
+++ cloudformation/create.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name Udagram --template-body file://./project_two.yml  --parameters file://./project_two.json --region=us-east-1
+aws cloudformation create-stack --stack-name Udagram --template-body file://project_two.yml  --parameters file://project_two.json --region=us-east-1
